f5a06be4d84d687899c47ab4898c33ffc4280369
Candace Wilson <candacewilson@Candaces-MacBook-Air.local>
2019-12-05 22:04:48 -0500

    end of night save

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+let button = document.querySelectorAll('.btn');
+button.forEach( element => {
+    element.addEventListener('click', () => {
+        element.style.backgroundColor = 'slateblue'; 
+    })
+})
+
+
+window.addEventListener("keydown", event => {
+    if (event.key == "y") {
+      document.body.style.background = "yellow";
+    }
+  });
+  window.addEventListener("keyup", event => {
+    if (event.key == "y") {
+      document.body.style.background = "";
+    }
+  });
+
+
